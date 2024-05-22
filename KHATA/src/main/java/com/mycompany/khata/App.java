c82c94b01cbb6d6fed0d9197e670e4d916fe1259
andresh <shahiandy123@gmail.com>
2024-05-22 19:10:06 +0545

    login

--- KHATA/src/main/java/com/mycompany/khata/App.java
+++ KHATA/src/main/java/com/mycompany/khata/App.java
@@ -33,7 +33,6 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
-
     public static void main(String[] args) {
         launch();
     }
---
+++ KHATA/src/main/java/com/mycompany/khata/RegisterController.java
@@ -0,0 +1,5 @@
+package com.mycompany.khata;
+
+public class RegisterController {
+    
+}
