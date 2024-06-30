1de70a13deb9b030cada6eafbd05c4e5ee829b25
andresh <shahiandy123@gmail.com>
2024-06-30 22:54:22 +0545

    default

--- KHATA/src/main/java/com/mycompany/khata/MainPageController.java
+++ KHATA/src/main/java/com/mycompany/khata/MainPageController.java
@@ -6,6 +6,7 @@ import javafx.scene.image.ImageView;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -16,4 +17,20 @@ public class MainPageController{
     @FXML
     private ImageView bankLogo;
 
+    @FXML
+    private void handleviewaccount(ActionEvent event) {
+        try {
+            App.setRoot("details");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void handletransferfunds(ActionEvent event) {
+        try {
+            App.setRoot("transfer");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
